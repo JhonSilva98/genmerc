fa95df61cf59c7f38228ee5053c49763789dd1b4
JhonSilva98 <jhonatansilva98@outlook.com>
2023-09-05 18:01:30 -0300

    flutter firedart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:genmerc/theme/theme.dart';
+import 'package:firedart/firedart.dart';
+
+/*const firebaseConfig = {
+  apiKey: "AIzaSyCLzhrdFXnEX6wYY5-uP5Cad7ce2Grvn6M",
+  authDomain: "genmerc-eb644.firebaseapp.com",
+  projectId: "genmerc-eb644",
+  storageBucket: "genmerc-eb644.appspot.com",
+  messagingSenderId: "597213776170",
+  appId: "1:597213776170:web:508f660129facd0e83b653",
+  measurementId: "G-Q5TJ96XR67"
+};*/
+const apiKey = "AIzaSyCLzhrdFXnEX6wYY5-uP5Cad7ce2Grvn6M";
+const projectId = "genmerc-eb644";
 
 void main() {
+  Firestore.initialize(projectId);
   runApp(const MyApp());
 }
